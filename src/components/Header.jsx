08758367aeccb6073b4b2be14de07e79f5676d65
NipulPanit <pnipul7678@gmail.com>
2025-05-05 23:53:30 +0530

    mobile completed one more section added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,17 +4,15 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <header className="w-full h-auto py-4 bg-white border-b border-primary/90 sticky top-0">
-      <nav className="flex justify-around items-center ">
-        <div className="">
-          <Link to="/">
-            <img
-              src="/eliteLogo.png"
-              alt="Elite Computer Institute"
-              className="sm:w-54 w-40"
-            />
-          </Link>
-        </div>
-        <ul className="flex gap-8 text-lg">
+      <nav className="flex sm:justify-around justify-between items-center sm:px-0 px-5">
+        <Link to="/">
+          <img
+            src="/eliteLogo.png"
+            alt="Elite Computer Institute"
+            className="sm:w-54 w-40"
+          />
+        </Link>
+        <ul className="sm:flex gap-8 text-lg hidden ">
           <li>
             <Link to="/">Home</Link>
           </li>
@@ -28,7 +26,8 @@ function Header() {
             <Link to="/contact">Contact</Link>
           </li>
         </ul>
-        <button className="bg-primary text-white px-6 py-2 rounded-full cursor-pointer">
+        <i className="ri-menu-2-line text-black text-2xl sm:hidden cursor-pointer"></i>
+        <button className="bg-primary text-white px-6 py-2 rounded-full cursor-pointer hidden sm:block">
           Enroll Now
         </button>
       </nav>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
-import Header from "../components/Header";
-import { Link } from "react-router-dom";
 
 function Home() {
   return (
@@ -45,11 +43,34 @@ function Home() {
       </Helmet>
 
       <main className="w-full h-full">
-        <div className="bg-[url(/1.jpeg)] bg-center bg-cover sm:p-10 p-5"> 
-          <h1 className="md:text-[5rem] sm:text-[3rem] text-3xl text-center flex flex-col p-10 py-30 text-black bg-white/60 container mx-auto rounded-full">
+        <div className="bg-[url(/1.jpeg)] bg-center bg-cover sm:p-10 p-5">
+          <h1 className="md:text-[4rem] sm:text-[3rem] text-3xl text-center flex flex-col px-10 sm:py-30  py-20 text-black bg-white/60 container mx-auto rounded-full">
             Unlock Your Learning Potential <span>Today! With</span>
-            <span className="underline decoration-secondary decoration-2 underline-offset-8 text-primary">Elite Computer Institute</span>
+            <span className="underline decoration-secondary decoration-2 underline-offset-8 text-primary">
+              Elite Computer Institute
+            </span>
           </h1>
+        </div>
+        <div className="w-full sm:py-20 py-7 sm:px-0 px-4 sm:container sm:mx-auto flex flex-col justify-center items-center">
+          <p className="sm:text-center sm:max-w-2/3 sm:leading-10 sm:text-2xl text-lg">
+            At{" "}
+            <span className="underline decoration-secondary decoration-1 underline-offset-2 text-primary">
+              {" "}
+              Elite Computer Institute
+            </span>
+            , we provide comprehensive computer training to help you succeed in
+            today’s digital world. Join our courses and transform your career.
+          </p>
+          <form className="w-full flex flex-col sm:flex-row gap-4 sm:mt-10 mt-5 justify-center items-center">
+            <input
+              type="text"
+              placeholder="Enter your Mobile Number"
+              className="border border-gray-300 px-4 py-2 rounded-full sm:w-2/6 outline-primary transition duration-200 ease-in-out focus:border-primary w-full"
+            />
+            <button className="bg-primary text-white px-6 py-2 rounded-full cursor-pointer">
+              Enquiry Now
+            </button>
+          </form>
         </div>
       </main>
     </>
