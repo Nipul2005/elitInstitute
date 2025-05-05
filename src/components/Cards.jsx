29b2ec68a1916ce024038e7591851a9e7b6d5b74
NipulPanit <pnipul7678@gmail.com>
2025-05-06 03:24:20 +0530

    litle changes

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -12,25 +12,11 @@ function Cards() {
             </h2>
           </div>
 
-          <p className="text-lg text-black p-2 pt-3">
+          <p className="text-md text-black p-2 pt-3">
             We have successfully trained over 100 students, equipping them with
             vital computer skills for the modern workforce.
           </p>
         </div>
-        <div className="flex-1 border border-primary">
-          <div className="flex gap-2 justify-start items-center pr-2">
-            <i className="ri-bookmark-3-line text-3xl bg-primary  w-12 h-12 flex justify-center items-center text-white"></i>
-            <h2 className="text-xl font-semibold text-primary">
-              10 years of experience
-            </h2>
-          </div>
-          <p className="text-lg text-black p-2 pt-3">
-            Our team boasts over 10 years of combined experience in computer
-            education, ensuring high-quality instruction.
-          </p>
-        </div>
-      </div>
-      <div className="flex sm:flex-row flex-col justify-start items-start gap-x-2 gap-y-2">
         <div className="flex-1 border border-primary">
           <div className="flex gap-2 justify-start items-center pr-2">
             <i className="ri-presentation-line text-3xl bg-primary  w-12 h-12 flex justify-center items-center text-white"></i>
@@ -39,9 +25,23 @@ function Cards() {
             </h2>
           </div>
 
-          <p className="text-lg text-black p-2 pt-3">
+          <p className="text-md text-black p-2 pt-3">
             Our dedicated instructors are committed to student success,
             providing personalized guidance and support.
+          </p>
+        </div>
+      </div>
+      <div className="flex sm:flex-row flex-col justify-start items-start gap-x-2 gap-y-2">
+        <div className="flex-1 border border-primary">
+          <div className="flex gap-2 justify-start items-center pr-2">
+            <i className="ri-bookmark-3-line text-3xl bg-primary  w-12 h-12 flex justify-center items-center text-white"></i>
+            <h2 className="text-xl font-semibold text-primary">
+              10 years of experience
+            </h2>
+          </div>
+          <p className="text-md text-black p-2 pt-3">
+            Our team boasts over 10 years of combined experience in computer
+            education, ensuring high-quality instruction.
           </p>
         </div>
         <div className="flex-1 border border-primary">
@@ -51,7 +51,7 @@ function Cards() {
               visionary leader
             </h2>
           </div>
-          <p className="text-lg text-black p-2 pt-3">
+          <p className="text-md text-black p-2 pt-3">
             Led by{" "}
             <span
               className="underline
